11675ac96618d269631ba682f26248649228b739
aliciabayerl <abayerl@uni-osnabrueck.de>
2023-08-04 14:02:07 +0200

    Plot for all data updated

--- Dataplots.py
+++ Dataplots.py
@@ -8,12 +8,12 @@ df2 = pd.read_csv('MC_cleaned_dataset.csv', sep=',')
 df2 = df2.drop('anemia_confirmed', axis=1)
 
 titles_df1 = ["Number of anemic participants", "Number of deceased participants", "Number of participants with fever", "Number of participants with high bloodpressure", "Number of participants with hypothermia", "Number of participants with low oxygen level", "Number of participants with respiratory distress", "Number of participants with abdominal pain", "Number of participants with appetite loss", "Number of participants with chest pain", "Number of participants with chills", "Number of participants with cough", "Number of participants with diarrhea", "Number of participants with fatigue", "Number of participants with headache", "Number of participants with nausea", "Number of participants with runny nose", "Number of participants with sore throat", "Number of participants with loss of taste or smell"]
-labeling_df1 = [['anemic', 'not anemic'], ['deceased', 'not deceased'], ['fever', 'no fever'], ['high bloodpressure', 'no high bloodpressure'], ['hypothermia', 'no hypothermia'], ['low oxygen level', 'normal oxygen level'], ['respiratory distress', 'no respiratory distress'], ['abdominal pain', 'no abdominal pain'], ['appetite loss', 'no appetite loss'], ['chest pain', 'no chest pain'], ['chills', 'no chills'], ['cough', 'no cough'], ['diarrhea', 'no diarrhea'], ['fatigue', 'no fatigue'], ['headache', 'no headache'], ['nausea', 'no nausea'], ['runny nose', 'no runny nose'], ['sore throat', 'no sore throat'], ['loss of taste or smell', 'no loss of taste or smell']]
+labeling_df1 = [['not anemic', 'anemic'], ['not deceased', 'deceased'], ['no fever', 'fever'], ['no high bloodpressure', 'high bloodpressure',], ['no hypothermia', 'hypothermia'], ['normal oxygen level', 'low oxygen level'], ['no respiratory distress','respiratory distress'], ['no abdominal pain', 'abdominal pain'], ['no appetite loss', 'appetite loss'], ['no chest pain', 'chest pain'], ['no chills', 'chills'], ['no cough', 'cough'], ['no diarrhea', 'diarrhea'], ['no fatigue', 'fatigue'], ['no headache', 'headache'], ['no nausea', 'nausea'], ['no runny nose', 'runny nose'], ['no sore throat', 'sore throat'], ['no loss of taste or smell', 'loss of taste or smell']]
 
 titles_df2 = ["Age of participants", "BMI of participants", "Number of deceased participants", "Number of participants that were hospitalized before", "Number of participants with asthma", "Number of participants with cardiac diseases", "Number of participants with diabetes", "Number of participants with HIV", "Number of participants with hypertension", "Number of participants with pulmonary diseases", "Sex of participants", "Number of participants that smoke", "Number of participants with uncontrolled diabetes", "Exposure risk of participants"]
-labeling_df2 = [['18-44', '<18', '45-64', '65+'], ['normal', 'underw.', 'overw.', 'obesity'], ['deceased', 'not deceased'], ['hospitalized before', 'not hospitalized before'], ['asthma', 'no asthma'], ['cardiac diseases', 'no cardiac diseases'], ['diabetes', 'no diabetes'], ['HIV pain', 'no HIV'], ['hypertension', 'no hypertension'], ['pulmonary diseases', 'no pulmonary diseases'], ['female', 'male'], ['smoking', 'not smoking'], ['uncontrolled diabetes', 'no uncontrolled diabetes'], ['exposure risk', 'no exposure risk']]
+labeling_df2 = [['18-44', '<18', '45-64', '65+'], ['underw.', 'normal', 'overw.', 'obesity'], ['not deceased','deceased'], ['not hospitalized before', 'hospitalized before'], ['no asthma','asthma'], ['no cardiac diseases','cardiac diseases'], ['no diabetes','diabetes'], ['no HIV','HIV pain'], ['no hypertension','hypertension'], ['no pulmonary diseases','pulmonary diseases'], ['male','female'], ['not smoking','smoking'], ['no uncontrolled diabetes','uncontrolled diabetes'], ['no exposure risk','exposure risk']]
 
-def plot_stacked_bar(df, titles, labeling):
+def plot_stacked_bar(df, titles, labeling, grouping, saveplot):
     num_plots = len(titles)
     num_cols = 4
     num_rows = (num_plots - 1) // num_cols + 1
@@ -21,14 +21,19 @@ def plot_stacked_bar(df, titles, labeling):
     
     for i in range(num_plots):
         ax = plt.subplot(num_rows, num_cols, i + 1)
-        data = df.groupby([df.iloc[:, i], 'deceased']).size().unstack()
+        plt.subplots_adjust(hspace=0.8, wspace=0.5)
+        data = df.groupby([df.iloc[:, i], grouping]).size().unstack()
         data.plot(kind='bar', stacked=True, ax=ax, color=['orange', 'blue'])
         ax.set_xticklabels(labeling[i], fontsize=7, rotation=0)
         ax.set_ylabel("Number of samples", fontsize=7)
         ax.set_title(titles[i], fontsize=7)
     
-    plt.tight_layout()
-    plt.show()
+    plt.savefig(saveplot)
 
-plot_stacked_bar(df, titles_df1, labeling_df1)
-plot_stacked_bar(df2, titles_df2, labeling_df2)
+    #plt.tight_layout()
+    #plt.show()
+
+
+
+plot_stacked_bar(df, titles_df1, labeling_df1, 'deceased', "Data1")
+plot_stacked_bar(df2, titles_df2, labeling_df2, 'deceased', "Data2")
--- feature_importances_plot.py
+++ feature_importances_plot.py
@@ -16,6 +16,6 @@ def subplot_feature_importances(data1, title1):
     plt.show()
 
 data_importance = pd.read_csv('feature_importances_casefile_hospitalized.csv')
-#data_importance2 = pd.read_csv('feature_importances2.csv')
+data_importance2 = pd.read_csv('feature_importances2.csv')
 
 subplot_feature_importances(data_importance, data_importance2, "Risk Factors for Dying from covid (Medical Conditions)", "Risk Factors for Dying from covid (Symptoms)")
