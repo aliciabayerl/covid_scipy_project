2d2e226978688aa1512cff664b365add8cef33ed
JanaLap <131407041+JanaLap@users.noreply.github.com>
2023-08-04 12:45:24 +0200

    Add files via upload

--- cleaning_data.py
+++ cleaning_data.py
@@ -4,6 +4,35 @@ from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv('df_riskfactormanuscript.csv')
 df1 = pd.read_csv('df_riskfactormanuscript.csv')
+reader = pd.read_csv("COVID-19_Case_Surveillance.csv", iterator=True, sep=',')
+df = reader.get_chunk(100000) #45000000 rf dauert dann sehr lange
+
+age_range_mapping_cf = {
+    "0 - 17 years": 0,
+    "18 to 49 years": 1,
+    "50 to 64 years": 2,
+    "65+ years": 3,
+}
+conditions_mapping_cf = {
+    "Yes": 1,
+    "No": 0
+}
+
+sex_mapping_cf = {
+    "Female": 0,
+    "Male": 1
+}
+exposure_mapping_cf = {
+    "Missing": 0,
+    "Unknown": 0,
+    "Yes": 1
+}
+mapping_cf = {
+    "No": 0,
+    "Yes": 1
+}
+
+
 
 columns_to_delete = [
     "fever", "low_oxygen94_enrollment", "age_years", "anemia", "anyinfectious", "bmi_adult", "bmi_obese", "country.x", "covidcasestatus_new", "exposure_workingoutsidehome", "form.case..case_id", "obs_appearance", "Region_collapsed", "Region_manuscript",
@@ -20,6 +49,10 @@ columns_to_delete2 = [
     "exposure_workingoutsidehome", "history_asthma", "history_cardiac", "history_chronic_cat", "history_diabetes",
     "history_hiv", "history_hypertension", "history_pulmonary", "history_tb", "form.case..case_id", "Region_collapsed",
     "Region_manuscript", "sex", "smoke", "Studysite_manuscript", "suspected_malaria", "symptoms_any", "test_reason", "uncontrolled_diabetes8", "symptoms_jointpain.x", "symptoms_wheezing.x"
+]
+columns_to_delete_cf = [
+    "case_month", "res_state", "state_fips_code", "res_county", "county_fips_code", "ethnicity", "case_positive_specimen_interval",
+    "case_onset_interval", "case_positive_specimen_interval", "case_onset_interval", "process", "symptom_status","race", "process"
 ]
 
 # Rename Columns
@@ -81,11 +114,37 @@ df1['low_oxygen'] = df1['low_oxygen'].replace('Normal oxygen level- above or equ
 df1['deceased'] = df1['deceased'].replace("deceased", 'yes')
 df1['covid'] = df1['covid'].replace("confirmed (rtpcr)", 'yes')
 
+    # Casefile
+df.drop(df[df['current_status'] == 'Probable Case'].index, inplace= True)
+df.drop(df[df['hosp_yn'] == 'Unknown'].index, inplace= True)
+df.drop(df[df['hosp_yn'] == 'Missing'].index, inplace= True)
+df.drop(df[df['icu_yn'] == 'Unknown'].index, inplace= True)
+df.drop(df[df['icu_yn'] == 'Missing'].index, inplace= True)
+df=df.dropna(subset = ['death_yn'])
+df.drop(df[df['death_yn'] == 'Unknown'].index, inplace= True)
+df.drop(df[df['death_yn'] == 'Missing'].index, inplace= True)
+df=df.dropna(subset = ['sex'])
+df=df.dropna(subset = ['age_group'])
+df['underlying_conditions_yn'] = df['underlying_conditions_yn'].fillna('No')
+df = df.drop(columns='current_status')
+
+
+
+
 # Mapping 
 
     # Medical Conditions
 data['age_categories'] = data['age_categories'].map(age_range_mapping)
 data['bmi_cat'] = data['bmi_cat'].map(bmi_mapping)
+
+    # Casefile
+df['age_group'] = df['age_group'].map(age_range_mapping_cf)
+df['exposure_yn'] = df['exposure_yn'].map(exposure_mapping_cf)
+df['sex'] = df['sex'].map(sex_mapping_cf)
+df['hosp_yn'] = df['hosp_yn'].map(mapping_cf)
+df['icu_yn'] = df['icu_yn'].map(mapping_cf)
+df['death_yn'] = df['death_yn'].map(mapping_cf)
+df['underlying_conditions_yn'] = df['underlying_conditions_yn'].map(conditions_mapping_cf)
 
 
 # Delete columns or rows 
@@ -103,6 +162,9 @@ df1.dropna(subset=['deceased'], axis=0, inplace=True)
 df1.drop(columns='covid', inplace=True)
 df1.drop(df1.columns[0], axis=1, inplace=True)
 
+    # Casefile
+df = df.drop(columns=columns_to_delete_cf)
+
 
 # Transform into numerical values
 data = data.replace({"yes": 1, "no": 0})
@@ -114,3 +176,4 @@ pd.set_option('display.max_columns', None)
 
 data.to_csv('mc_cleaned_dataset.csv', index=False)
 df1.to_csv('sy_cleaned_dataset.csv', index=False)
+df.to_csv('casefile_breinigt.csv', index=False)
