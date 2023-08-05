488516f9c79d5bf4b5bf526b73bee14e28b410a5
aliciabayerl <abayerl@uni-osnabrueck.de>
2023-08-05 15:57:58 +0200

    Final Folder Structure / Organization Update

--- models_cf.py
+++ models_cf.py
@@ -1,14 +1,17 @@
 ## import libraries, functions from different files
 import pandas as pd
-from logistic_regression import logistic_regression
-from randomForest import random_forest
-from decisionTree import decision_tree
-from knn_classification import knn_classification
 import matplotlib.pyplot as plt
-from kmeans import kmeans_clustering
+import os
+from ScriptsModels import randomForest
 
+
+current_dir = os.getcwd()
+datasets_folder = 'Datasets'  
+
+# Load data from the Datasets folder
+data = pd.read_csv(os.path.join(datasets_folder, 'cf_cleaned_dataset.csv'))
 ## load cleaned casefile dataset
-data = pd.read_csv('cf_cleaned_dataset.csv')
+#data = pd.read_csv('cf_cleaned_dataset.csv')
 # Separate the features and the target variables
 X = data[["age_group", "sex", "exposure_yn", "underlying_conditions_yn"]]
 y1 =data['hosp_yn']
@@ -19,11 +22,11 @@ y3 =data['death_yn']
 #here only RF is used, due to the fact, that RF performed best with the medical conditions dataset
 # Train the RF for the different dependent variables and get the results
 # for hospitalized people
-rf1_model, feature1_importancesRF, rf1_fpr, rf1_tpr, rf1_auc = random_forest(X, y1)
+rf1_model, feature1_importancesRF, rf1_fpr, rf1_tpr, rf1_auc = randomForest.random_forest(X, y1)
 # for people in ICU
-rf2_model, feature2_importancesRF, rf2_fpr, rf2_tpr, rf2_auc = random_forest(X, y2)
+rf2_model, feature2_importancesRF, rf2_fpr, rf2_tpr, rf2_auc = randomForest.random_forest(X, y2)
 # for people that died
-rf3_model, feature3_importancesRF, rf3_fpr, rf3_tpr, rf3_auc = random_forest(X, y3)
+rf3_model, feature3_importancesRF, rf3_fpr, rf3_tpr, rf3_auc = randomForest.random_forest(X, y3)
 
 ## general predefinition for the whole plot
 plt.figure(figsize=(14, 6))
@@ -58,20 +61,26 @@ plt.legend(loc='lower right')
 ## Plot the data
 plt.tight_layout()
 # Plot speichern
-plt.savefig('casefile_RF_ROC_Curves')
 
-## Calculate the featureimportance for each predicted variable
+# Save the plot in the 'Plots' folder
+plot_filename = os.path.join('Plots', 'casefile_RF_ROC_Curves.png')
+plt.savefig(plot_filename)
+
+# Calculate the feature importance for each predicted variable
 # Feature importances RF for hospitalized people
 feature1_importancesRF = pd.DataFrame({'Feature': X.columns, 'Importance': feature1_importancesRF})
 feature1_importancesRF = feature1_importancesRF.sort_values(by='Importance', ascending=False)
-feature1_importancesRF.to_csv('feature_importances_cf_hospitalized.csv', index=False)
+feature1_importances_filename = os.path.join('FeatureImportances', 'feature_importances_cf_hospitalized.csv')
+feature1_importancesRF.to_csv(feature1_importances_filename, index=False)
 
 # Feature importances RF for people in ICU
 feature2_importancesRF = pd.DataFrame({'Feature': X.columns, 'Importance': feature2_importancesRF})
 feature2_importancesRF = feature2_importancesRF.sort_values(by='Importance', ascending=False)
-feature2_importancesRF.to_csv('feature_importances_cf_icu.csv', index=False)
+feature2_importances_filename = os.path.join('FeatureImportances', 'feature_importances_cf_icu.csv')
+feature2_importancesRF.to_csv(feature2_importances_filename, index=False)
 
-# Feature importances RRF for people that died
+# Feature importances RF for people that died
 feature3_importancesRF = pd.DataFrame({'Feature': X.columns, 'Importance': feature3_importancesRF})
 feature3_importancesRF = feature3_importancesRF.sort_values(by='Importance', ascending=False)
-feature3_importancesRF.to_csv('feature_importances_cf_dead.csv', index=False)
+feature3_importances_filename = os.path.join('FeatureImportances', 'feature_importances_cf_dead.csv')
+feature3_importancesRF.to_csv(feature3_importances_filename, index=False)
